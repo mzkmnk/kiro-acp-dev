9bd3e0731c545d73bfb7f67fbf80901a73c6ca2c
mzkmnk <mzk.mnk.dev@gmail.com>
2026-03-02 12:32:40 +0900

    feat: プロジェクトセットアップ（ビルド環境構築）

---
+++ src/extension.ts
@@ -0,0 +1,9 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log("kiro-acp-dev: activated");
+}
+
+export function deactivate() {
+  console.log("kiro-acp-dev: deactivated");
+}
